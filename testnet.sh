e4fae2d578e42b821b6063a0c3102e17d3e91711
alpha-omega-labs <88520218+alpha-omega-labs@users.noreply.github.com>
2021-10-04 10:57:44 -0400

    Update testnet.sh

--- testnet.sh
+++ testnet.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo Hello, how do you do? Lets set you up for a train ride!                                    
-sleep 5s
+sleep 3s
 sudo apt-get update -y
 sudo apt-get install sl
 sudo apt-get install wget -y
 sudo apt-get install snap -y
 snap install jq
 echo Getin! Welcome on board! Installing your full node @ Genesis L1 testnet!
-sleep 5s
+sleep 3s
 sl -l
 cd
 wget https://github.com/tharsis/ethermint/releases/download/v0.5.0/ethermint_0.5.0_Linux_x86_64.tar.gz
@@ -15,7 +15,7 @@ tar -xf ethermint_0.5.0_Linux_x86_64.tar.gz
 cd bin
 ./ethermintd version
 echo Its alive, now you have ethermint v0.5.0
-sleep 5s
+sleep 3s
 ./ethermintd config keyring-backend test
 ./ethermintd config chain-id genesis_9000-1
 ./ethermintd keys add mygenesiskey --keyring-backend test --algo eth_secp256k1
@@ -29,14 +29,14 @@ cat $HOME/.ethermintd/config/genesis.json | jq '.consensus_params["block"]["max_
 ./ethermintd gentx mygenesiskey 1000000000000000000000aphoton --keyring-backend test --chain-id genesis_9000-1
 ./ethermintd collect-gentxs
 ./ethermintd validate-genesis
-echo Starting your node first time in local mode, letting it work 10s, than stopping it! 
+echo Starting your node first time in local mode, letting it work 3s, than stopping it! 
 ./ethermintd start --pruning=nothing --trace --log_level info --minimum-gas-prices=0aphoton --json-rpc.api eth,txpool,personal,net,debug,web3,miner &
 ethermintd_pid=$!
 kill $ethermintd_pid
 echo Node stop for setup!
-sleep 5s
+sleep 3s
 echo Starting some preparations before joining public network: adding peers, seeds, genesis.json and some LOVE!
-sleep 5s
+sleep 3s
 cd
 cd .ethermintd/data
 find -regextype posix-awk ! -regex './(priv_validator_state.json)' -print0 | xargs -0 rm -rf
@@ -48,8 +48,8 @@ wget https://raw.githubusercontent.com/alpha-omega-labs/bashscripts/main/genesis
 cd
 cd bin
 echo All set! 
-sleep 5s
+sleep 3s
 sl -l 
 echo Ready, steady, GO!!!  
-sleep 5s
+sleep 3s
 ./ethermintd start --pruning=nothing --trace --log_level info --minimum-gas-prices=0aphoton --json-rpc.api eth,txpool,personal,net,debug,web3,miner
